5e1387cef9dd4156d6875864040d7bfc342dffac
Paul <paul.tibbetts@stickee.co.uk>
2022-12-14 12:20:07 +0000

    fix: paths inside installed PHPStan config

--- app/Commands/InstallCommand.php
+++ app/Commands/InstallCommand.php
@@ -29,7 +29,7 @@ class InstallCommand extends Command
     public function handle(): int
     {
         if ($this->argument('directory') !== self::DEFAULT_INSTALL_DIRECTORY) {
-            $this->destination = $this->argument('directory');
+            $this->destination = rtrim($this->argument('directory'), '/') . DIRECTORY_SEPARATOR;
         }
 
         render(<<<'HTML'
@@ -64,6 +64,7 @@ class InstallCommand extends Command
 
         $this->task('Copy Larastan config', function () {
             $this->copyFilesFromVendorDirectory('stickee/larastan-config/dist');
+            return $this->amendLarastanConfig();
         });
 
         $this->task('Copy Rector config', function () {
@@ -89,26 +90,48 @@ class InstallCommand extends Command
 
     private function ensureDestinationDirectoryExists(): bool
     {
-        if ($this->destination !== '' && $this->destination !== '0') {
-            $success = true;
-            $disk = Storage::disk('cwd');
+        $success = true;
+        $disk = Storage::disk('cwd');
 
-            if (! $disk->exists($this->destination)) {
-                $success = false;
-            }
-
-            $this->task('Destination directory exists', static fn (): bool => $success);
-
-            if (! $success) {
-                render(<<<'HTML'
-                        <div class="py-1">
-                            <div class="bg-red-500">The destination directory must exist to copy files to.</div>
-                        </div>
-                    HTML);
-
-                return false;
-            }
+        if (! $disk->exists($this->destination)) {
+            $success = false;
         }
+
+        $this->task('Destination directory exists', static fn (): bool => $success);
+
+        if (! $success) {
+            render(<<<'HTML'
+                    <div class="py-1">
+                        <div class="bg-red-500">The destination directory must exist to copy files to.</div>
+                    </div>
+                HTML);
+        }
+
+        return $success;
+    }
+
+    private function amendLarastanConfig(): bool
+    {
+        $larastanConfig = 'phpstan.dist.neon';
+
+        $disk = Storage::disk('cwd');
+        $path = $this->destination . $larastanConfig;
+
+        if (! $disk->exists($path)) {
+            $this->newLine();
+            $this->error("Something went wrong.");
+            $this->error("Could not amend your {$larastanConfig} file.");
+            return false;
+        }
+
+        $original = $disk->get($path);
+
+        $new = Str::of($original)->replaceMatches(
+            pattern: '/tools\/phpstan\//m',
+            replace: 'tools/canary/'
+        );
+
+        $disk->put($path, (string) $new);
 
         return true;
     }
@@ -118,7 +141,7 @@ class InstallCommand extends Command
         $gitignore = '.gitignore';
         $amendedGitIgnore = $this->task('.gitignore includes .php-cs-fixer.cache', function () use ($gitignore) {
             $disk = Storage::disk('cwd');
-            $path = $this->destination !== '' && $this->destination !== '0' ? $this->destination . DIRECTORY_SEPARATOR . $gitignore : $gitignore;
+            $path = $this->destination . $gitignore;
 
             if (! $disk->exists($path)) {
                 return false;
@@ -135,7 +158,7 @@ class InstallCommand extends Command
         });
 
         if (! $amendedGitIgnore) {
-            $this->line('');
+            $this->newLine();
             $this->error("Could not find your {$gitignore} file.");
             $this->error("Please create a {$gitignore} file or ensure you are installing into the correct directory.");
         }
@@ -163,8 +186,8 @@ class InstallCommand extends Command
             $dest = $src;
         }
 
-        if ($this->destination !== '' && $this->destination !== '0') {
-            $dest = $this->destination . DIRECTORY_SEPARATOR . $dest;
+        if ($this->destination !== '') {
+            $dest = $this->destination . $dest;
         }
 
         Storage::disk('cwd')->put($dest, Storage::disk($disk)->get($src));
