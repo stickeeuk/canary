a947a8ffc57510d7c99166621f39a2cddbb5bff0
Oliver Earl <oliver.earl@stickee.co.uk>
2023-01-03 15:25:00 +0000

    refactor: Move logic into its own helper file to avoid violating PSR.

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,19 +1,6 @@
 <?php
 
-if (! function_exists('vendor_path')) {
-    function vendor_path(string $path = ''): string {
-        if (! empty($GLOBALS['_composer_bin_dir'])) {
-            $vendorPath = $GLOBALS['_composer_bin_dir'] . DIRECTORY_SEPARATOR;
-            $vendorPath = str_replace(DIRECTORY_SEPARATOR . 'bin', '', $vendorPath);
-        } else {
-            $vendorPath = base_path(DIRECTORY_SEPARATOR . 'vendor');
-        }
-
-        return $path
-            ? $vendorPath . DIRECTORY_SEPARATOR . $path
-            : $vendorPath;
-    }
-}
+declare(strict_types=1);
 
 return [
     'default' => 'local',
@@ -24,11 +11,11 @@ return [
         ],
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app')
+            'root' => storage_path('app'),
         ],
         'vendor' => [
             'driver' => 'local',
-            'root' => vendor_path()
-        ]
+            'root' => vendor_path(),
+        ],
     ],
 ];
