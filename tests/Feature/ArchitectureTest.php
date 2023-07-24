372566c01f35d513f7a2cfd946da85bea6095e8b
Oliver Earl <oliver.earl@stickee.co.uk>
2023-07-24 13:01:52 +0100

    test: Add architecture test.

---
+++ tests/Feature/ArchitectureTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Commands\InstallCommand;
+use App\Commands\ToolCommand;
+
+test('strict types are used in the project')
+    ->expect('App')
+    ->toUseStrictTypes();
+
+test('canary commands should extend the tool command class')
+    ->expect('App\Commands')
+    ->classes()
+    ->toExtend(ToolCommand::class)
+    ->ignoring([InstallCommand::class, ToolCommand::class]);
+
+test('there are no debugging methods present in the source code')
+    ->expect(['dd', 'var_dump', 'echo', 'ray', 'die', 'exit', 'dump', 'print_r'])
+    ->not()
+    ->toBeUsed();
