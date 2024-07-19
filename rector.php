f1f6e492c2f6a774672eb141617820cb705a8bb6
Oliver Earl <77276885+stickeeoliver@users.noreply.github.com>
2024-07-19 11:51:53 +0100

    ci: Resolve issues with Laravel 11 and PHP 8.3 (#56)
    * ci: Limit to PHP 8.3.
    
    * chore(deps): Use Rector v3.
    
    * chore: Update Rector config.
    
    * chore(deps): Enforce 8.3 usage.
    
    * ci: Update workflow
    
    * chore: add pest artifact to gitignore
    
    * fix: wrong directory
    
    * chore: remove cache as we're not using it
    
    * chore: yaml and yml
    
    * style(ci): fix indentation
    
    * chore(deps): Update minimum required versions to the latest
    
    * ci: Don't need extensions

--- rector.php
+++ rector.php
@@ -2,34 +2,28 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
 use RectorLaravel\Set\LaravelSetList;
 use RectorLaravel\Set\LaravelLevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/app'
-    ]);
-
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::TYPE_DECLARATION,
-        LevelSetList::UP_TO_PHP_81,
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . '/app'])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+    )
+    ->withSets([
         LaravelSetList::LARAVEL_CODE_QUALITY,
-        LaravelLevelSetList::UP_TO_LARAVEL_90,
+        LaravelLevelSetList::UP_TO_LARAVEL_110,
+    ])
+    ->withSkip([
+        EncapsedStringsToSprintfRector::class, // changes "th{$is}" to sprintf('th%s', 'is')
+        MixedTypeRector::class, // removes docblocks
+        PostIncDecToPreIncDecRector::class, // changes $i++ to ++$i
     ]);
-
-    $rectorConfig->skip([
-        // \Rector\TypeDeclaration\Rector\FunctionLike\ReturnTypeDeclarationRector::class, // Adds return types, which may conflict with Laravel built-ins.
-        \Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class, // Removes @param from docblocks.
-        \Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class, // Removes return from docblocks.
-        \Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector::class, // Changes $i++ to ++$i.
-        \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class, // Changes "th{$is}" to sprintf('th%s', 'is').
-        \Rector\Php80\Rector\FunctionLike\MixedTypeRector::class, // Removes docblocks.
-    ]);
-
-};
