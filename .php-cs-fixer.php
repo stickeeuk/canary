ca59851d18d940a41985dbeaf316248fc6591bb0
Paul Tibbetts <paul.tibbetts@stickee.co.uk>
2025-02-03 16:53:51 +0000

    chore: update own PHP CS Fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,15 +2,12 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 use Stickee\PhpCsFixerConfig;
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__ . '/app')
-    ->in(__DIR__ . '/config')
-    ->in(__DIR__ . '/tests')
-    ->append([
-        __DIR__ . '/.php-cs-fixer.dist.php',
-    ]);
+    ->in(__DIR__)
+    ;
 
 $overrideRules = [
     'ordered_class_elements' => ['order' => ['use_trait']],
@@ -24,5 +21,7 @@ $config = PhpCsFixerConfig\Factory::fromRuleSet(
 $config
     ->setFinder($finder)
     ->setCacheFile(__DIR__ . '/.php-cs-fixer.cache');
+
+$config->setParallelConfig(ParallelConfigFactory::detect());
 
 return $config;
